fe7dd5aa4f9f16eeec3d985719ffb3b841df8c32
n1k1_tos1_na <basharov100@gmail.com>
2019-05-04 22:42:18 +0300

    implemented Robots & TestRobots

--- task10/test/TestRobots.hs
+++ task10/test/TestRobots.hs
@@ -9,9 +9,67 @@ main = defaultMain testsRobots
 testsRobots :: TestTree
 testsRobots = let
         walter = robot "Walter" 50 50
+        karasek = robot "Beep" 50 67
+        karasik = robot "Wryy" 17 150
+        glassCannon = robot "GC" 100 5
+        deadbot = robot "DED" 5 0
+        tank = robot "TANK" 5 100
+        
     in testGroup "Unit tests for Robots task"
         [ testCase "Test for getName" $
             getName walter @?= "Walter"
+
+        , testCase "Test for getAttack" $
+            getAttack walter @?= 50
+
+        , testCase "Test for getHealth" $
+            getHealth walter @?= 50
+
+        , testCase "Test for setName" $
+            setName "Karasek" walter @?= ("Karasek", 50, 50)
+
+        , testCase "Test for setAttack" $
+            setAttack 9000 walter @?= ("Walter", 9000, 50)
+
+        , testCase "Test for setHealth" $
+            setHealth 9000 walter @?= ("Walter", 50, 9000)
+
         , testCase "Test for printRobot" $
             printRobot walter @?= "Walter, attack: 50, health: 50"
+
+        , testCase "Test for damage" $
+            (getHealth $ damage walter 9000) @?= -8950
+
+        , testCase "isAlive works on alive robot" $
+            isAlive walter @?= True
+
+        , testCase "isAlive works on dead robot" $
+            isAlive deadbot @?= False
+
+        , testCase "Alive robot fights" $
+            (getHealth $ fight walter karasek) @?= 17
+
+        , testCase "Dead robot fights" $
+            (getHealth $ fight deadbot karasek) @?= 67
+
+        , testCase "Attacker knockouts defender in first round" $
+            threeRoundFight glassCannon karasek @?= ("GC", 100, 5)
+
+        , testCase "Defender knockouts attacker in second round" $
+            threeRoundFight walter karasek @?= ("Beep", 50, 17)
+
+        , testCase "Attacker wins, took 1 hit" $
+            threeRoundFight karasek tank @?= ("Beep", 50, 62)
+
+        , testCase "Defender wins, took 2 hits" $
+            threeRoundFight tank karasek @?= ("Beep", 50, 57)
+
+        , testCase "Attacker wins, equal hp" $
+            threeRoundFight karasek karasik @?= ("Beep", 50, 50)
+
+        , testCase "Test for neueRobotAttack" $
+            (getHealth $ neueRobotAttack tank) @?= 91
+            
+        , testCase "Test for survivors" $
+            survivors @?= [robot "Strelizia" 1 1]
         ]
