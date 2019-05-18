5c2b82465ac358a6cf79f13ace2e852a7b2c3be0
n1k1_tos1_na <basharov100@gmail.com>
2019-05-18 20:41:53 +0300

    delete some spaces

--- task11/src/Map.hs
+++ task11/src/Map.hs
@@ -3,76 +3,75 @@
 -}
 module Map where
 
-  import Data.Maybe (isJust, maybe)
-  
-  {-|
-    Поведение всех определённых здесь функций должно быть аналогично
-    поведению функций из модуля "Data.Map.Strict".
-  
-    Каждую функцию, у которой здесь предложена реализация по умолчанию
-    в виде 'undefined', вам требуется выразить через другую функцию из
-    класса 'Map', указанную в комментарии. Например, 'fromList'
-    требуется выразить через 'insert' (и, возможно, какие-то другие
-    стандартные функции).
-  
-    Оставшиеся шесть функций считаются минимальной реализацией.
-  
-    Обратите внимание, что имена функций @lookup@ и @null@ совпадают
-    с определёнными в стандартной библиотеке, поэтому для обращения к ним
-    требуется писать @Map.lookup@ и @Map.null@, иначе компилятор не поймёт,
-    какую из двух функций вы хотите.
-  
-    Строго говоря, 'alter' и 'Map.lookup' можно обобщить до функции
-    вроде 'Data.Map.Strict.alterF', которая позволяет при изменении
-    'Map' ещё и вытащить наружу старое значение, но мы этим заниматься
-    не будем.
-  -}
-  class Map t where
-      empty :: Ord k => t k a
-  
-      singleton :: k -> a -> t k a
-  
-      fromList :: Ord k => [(k, a)] -> t k a
-      fromList = foldl (flip (uncurry insert)) empty
-  
-      toAscList :: t k a -> [(k, a)]
-  
-      insert :: Ord k => k -> a -> t k a -> t k a
-      insert = insertWith const
-  
-      insertWith :: Ord k => (a -> a -> a) -> k -> a -> t k a -> t k a
-      insertWith f k v = alter (Just . maybe v (f v)) k
-  
-      insertWithKey :: Ord k => (k -> a -> a -> a) -> k -> a -> t k a -> t k a
-      insertWithKey f k = insertWith (f k) k
-  
-      delete :: Ord k => k -> t k a -> t k a
-      delete = alter (const Nothing)
-  
-      adjust :: Ord k => (a -> a) -> k -> t k a -> t k a
-      adjust f = alter (fmap f)
-  
-      adjustWithKey :: Ord k => (k -> a -> a) -> k -> t k a -> t k a
-      adjustWithKey f k = adjust (f k) k
-  
-      update :: Ord k => (a -> Maybe a) -> k -> t k a -> t k a
-      update f = alter (maybe Nothing f)
-  
-      updateWithKey :: Ord k => (k -> a -> Maybe a) -> k -> t k a -> t k a
-      updateWithKey f k = update (f k) k
-  
-      alter :: Ord k => (Maybe a -> Maybe a) -> k -> t k a -> t k a
-  
-      lookup :: Ord k => k -> t k a -> Maybe a
-  
-      member :: Ord k => k -> t k a -> Bool
-      member k = isJust . Map.lookup k
-  
-      notMember :: Ord k => k -> t k a -> Bool
-      notMember k = not . member k
-  
-      null :: t k a -> Bool
-      null = (0 == ) . size
-  
-      size :: t k a -> Int
-  
+import Data.Maybe (isJust, maybe)
+
+{-|
+  Поведение всех определённых здесь функций должно быть аналогично
+  поведению функций из модуля "Data.Map.Strict".
+
+  Каждую функцию, у которой здесь предложена реализация по умолчанию
+  в виде 'undefined', вам требуется выразить через другую функцию из
+  класса 'Map', указанную в комментарии. Например, 'fromList'
+  требуется выразить через 'insert' (и, возможно, какие-то другие
+  стандартные функции).
+
+  Оставшиеся шесть функций считаются минимальной реализацией.
+
+  Обратите внимание, что имена функций @lookup@ и @null@ совпадают
+  с определёнными в стандартной библиотеке, поэтому для обращения к ним
+  требуется писать @Map.lookup@ и @Map.null@, иначе компилятор не поймёт,
+  какую из двух функций вы хотите.
+
+  Строго говоря, 'alter' и 'Map.lookup' можно обобщить до функции
+  вроде 'Data.Map.Strict.alterF', которая позволяет при изменении
+  'Map' ещё и вытащить наружу старое значение, но мы этим заниматься
+  не будем.
+-}
+class Map t where
+    empty :: Ord k => t k a
+
+    singleton :: k -> a -> t k a
+
+    fromList :: Ord k => [(k, a)] -> t k a
+    fromList = foldl (flip (uncurry insert)) empty
+
+    toAscList :: t k a -> [(k, a)]
+
+    insert :: Ord k => k -> a -> t k a -> t k a
+    insert = insertWith const
+
+    insertWith :: Ord k => (a -> a -> a) -> k -> a -> t k a -> t k a
+    insertWith f k v = alter (Just . maybe v (f v)) k
+
+    insertWithKey :: Ord k => (k -> a -> a -> a) -> k -> a -> t k a -> t k a
+    insertWithKey f k = insertWith (f k) k
+
+    delete :: Ord k => k -> t k a -> t k a
+    delete = alter (const Nothing)
+
+    adjust :: Ord k => (a -> a) -> k -> t k a -> t k a
+    adjust f = alter (fmap f)
+
+    adjustWithKey :: Ord k => (k -> a -> a) -> k -> t k a -> t k a
+    adjustWithKey f k = adjust (f k) k
+
+    update :: Ord k => (a -> Maybe a) -> k -> t k a -> t k a
+    update f = alter (maybe Nothing f)
+
+    updateWithKey :: Ord k => (k -> a -> Maybe a) -> k -> t k a -> t k a
+    updateWithKey f k = update (f k) k
+
+    alter :: Ord k => (Maybe a -> Maybe a) -> k -> t k a -> t k a
+
+    lookup :: Ord k => k -> t k a -> Maybe a
+
+    member :: Ord k => k -> t k a -> Bool
+    member k = isJust . Map.lookup k
+
+    notMember :: Ord k => k -> t k a -> Bool
+    notMember k = not . member k
+
+    null :: t k a -> Bool
+    null = (0 == ) . size
+
+    size :: t k a -> Int
--- task11/src/NaiveTree.hs
+++ task11/src/NaiveTree.hs
@@ -4,70 +4,69 @@
   время в худшем случае.
 -}
 module NaiveTree where
-  import Map
-  
-  {-|
-    Двоичное дерево поиска, необязательно сбалансированное.
-  
-    Инвариант: для любой вершины @v@:
-  
-    1. Все ключи в левом поддереве строго меньше ключа @v@.
-    2. Все ключи в правом поддереве строго больше ключа @v@.
-  -}
-  data NaiveTree k a =
-      -- |Пустое дерево
-      Nil
-      -- |@Node k a l r@ – дерево с корнем в вершине с ключом @k@,
-      -- значением @a@, левым ребёнком @l@ и правым ребёнком @r@.
-    | Node k a (NaiveTree k a) (NaiveTree k a)
-    deriving (Show, Eq)
-  
-  {-|
-    @merge l r@ объединяет два дерева в одно при условии,
-    что все ключи из @l@ строго меньше ключей из @r@.
-  -}
-  merge :: NaiveTree k a -> NaiveTree k a -> NaiveTree k a
-  merge Nil r'            = r'
-  merge (Node k a l r) r' = Node k a l (merge r r')
-  
-  {-|
-    Реализация функций 'Map' для 'NaiveTree'.
-  
-    'empty', 'singleton' и 'Map.null' работают за /O(1)/.
-    Если /n/ – количество вершин дерева, а /h/ – высота дерева,
-    то 'fromList' работает за /O(nh)/, 'toAscList' работает за /O(n^2)/,
-    а 'size' работает за /O(n)/.
-    Остальные функции работают за /O(h)/,
-    причём каждая функция должна спускаться вниз по дереву и
-    подниматься обратно не больше одного раза.
-  
-    Скорее всего, при реализации вам потребуется функция 'merge'.
-  -}
-  instance Map NaiveTree where
-      empty = Nil
-  
-      singleton k a = Node k a Nil Nil
-  
-      toAscList Nil            = []
-      toAscList (Node k a l r) = toAscList l ++ [(k, a)] ++ toAscList r
-  
-      alter f k Nil = maybe empty (singleton k) (f Nothing)
-      alter f k' (Node k a l r)
-          | k' < k    = Node k a (alter f k' l) r
-          | k' > k    = Node k a l (alter f k' r)
-          | otherwise = let
-              r' = maybe r (\a' -> merge (singleton k a') r) (f (Just a))
-              in merge l r'
-  
-      lookup _ Nil = Nothing
-      lookup k' (Node k a l r)
-          | k' < k    = Map.lookup k' l
-          | k' > k    = Map.lookup k' r
-          | otherwise = Just a
-  
-      null Nil = True
-      null _   = False
-  
-      size Nil            = 0
-      size (Node _ _ l r) = size l + 1 + size r
-  
+import Map
+
+{-|
+  Двоичное дерево поиска, необязательно сбалансированное.
+
+  Инвариант: для любой вершины @v@:
+
+  1. Все ключи в левом поддереве строго меньше ключа @v@.
+  2. Все ключи в правом поддереве строго больше ключа @v@.
+-}
+data NaiveTree k a =
+    -- |Пустое дерево
+    Nil
+    -- |@Node k a l r@ – дерево с корнем в вершине с ключом @k@,
+    -- значением @a@, левым ребёнком @l@ и правым ребёнком @r@.
+  | Node k a (NaiveTree k a) (NaiveTree k a)
+  deriving (Show, Eq)
+
+{-|
+  @merge l r@ объединяет два дерева в одно при условии,
+  что все ключи из @l@ строго меньше ключей из @r@.
+-}
+merge :: NaiveTree k a -> NaiveTree k a -> NaiveTree k a
+merge Nil r'            = r'
+merge (Node k a l r) r' = Node k a l (merge r r')
+
+{-|
+  Реализация функций 'Map' для 'NaiveTree'.
+
+  'empty', 'singleton' и 'Map.null' работают за /O(1)/.
+  Если /n/ – количество вершин дерева, а /h/ – высота дерева,
+  то 'fromList' работает за /O(nh)/, 'toAscList' работает за /O(n^2)/,
+  а 'size' работает за /O(n)/.
+  Остальные функции работают за /O(h)/,
+  причём каждая функция должна спускаться вниз по дереву и
+  подниматься обратно не больше одного раза.
+
+  Скорее всего, при реализации вам потребуется функция 'merge'.
+-}
+instance Map NaiveTree where
+    empty = Nil
+
+    singleton k a = Node k a Nil Nil
+
+    toAscList Nil            = []
+    toAscList (Node k a l r) = toAscList l ++ [(k, a)] ++ toAscList r
+
+    alter f k Nil = maybe empty (singleton k) (f Nothing)
+    alter f k' (Node k a l r)
+        | k' < k    = Node k a (alter f k' l) r
+        | k' > k    = Node k a l (alter f k' r)
+        | otherwise = let
+            r' = maybe r (\a' -> merge (singleton k a') r) (f (Just a))
+            in merge l r'
+
+    lookup _ Nil = Nothing
+    lookup k' (Node k a l r)
+        | k' < k    = Map.lookup k' l
+        | k' > k    = Map.lookup k' r
+        | otherwise = Just a
+
+    null Nil = True
+    null _   = False
+
+    size Nil            = 0
+    size (Node _ _ l r) = size l + 1 + size r
